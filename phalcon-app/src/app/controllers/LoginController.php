7ae05a2da6c9700dd04432ea2466dac23b029669
AnshumanRai-cedcoss <anshumanrai@cedcoss.com>
2022-03-16 09:42:19 +0530

    first commit

--- phalcon-app/src/app/controllers/LoginController.php
+++ phalcon-app/src/app/controllers/LoginController.php
@@ -8,20 +8,26 @@ class LoginController extends Controller
     {
       
         
-        $this->view->users = Users::find();
+        // $this->view->users = Users::find();
         // return '<h1>Hello World!</h1>';
     }
 
-    public function registerAction(){
-
-        // $robots = $connection->fetchAll(
-        //     "SELECT * FROM users",
-        //     \Phalcon\Db::FETCH_ASSOC
-        // );   
-        // print_r($robots); 
-    //  print_r($this->request->getPost());
-    //  die();
-        $res = $this->request->getPost() ;
-         
+    public function registerAction(){ 
+      $data = $this->request->getPost();
+           $res = Users::find();
+        //    echo "Helo";
+           foreach ($res as $k => $v) {
+              if($v->email == $data["email"] && $v->password == $data["password"])
+              {
+                  
+                header("location:success");
+              }
+           }
+        // $res = $this->request->getPost() ;
+        return "Incorrect email or password";
+   }
+   public function successAction()
+   {
+    $this->view->users = Users::find();
    }
 }
